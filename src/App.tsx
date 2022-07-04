1149823a72ae477226ee8b863ad345e651043c99
katiescruggs <katie.e.scruggs@gmail.com>
2022-07-04 12:10:59 -0600

    Create react app and set up react router

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { Link, Routes, Route } from "react-router-dom";
+import Gauge from "./components/Gauge";
+import Blog from "./components/Blog";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Bookkeeper!</h1>
+      <nav 
+        style={{borderBottom: "solid 1px", paddingBottom: "1rem",}}>
+          <Link to="/gauge">Gauge</Link>
+          <Link to="/blog">Blog</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Blog />}/>
+        <Route path="gauge" element={<Gauge />}/>
+        <Route path="blog" element={<Blog />} />
+      </Routes>
+    </div>
+  );
+} 
---
+++ src/components/Gauge.tsx
@@ -0,0 +1,5 @@
+export default function Gauge() {
+  return (
+    <div>Gauge</div>
+  );
+}
